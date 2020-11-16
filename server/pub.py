8f2b46275702405b43ae07a1c3ad63b348309287
daddy <ericwu@daddy.local>
2020-11-15 22:20:48 -0800

    [Eric ] Server

--- server/pub.py
+++ server/pub.py
@@ -1,11 +1,17 @@
 # pip3 install paho-mqtt
 import paho.mqtt.client as paho
+import time
+
 broker = "54.90.30.207"
 port = 1883
 def on_publish(client,userdata,result):             #create function for callback
     print("data published \n")
     pass
+
 client1= paho.Client("control1")                           #create client object
 client1.on_publish = on_publish                          #assign function to callback
 client1.connect(broker,port)                                 #establish connection
-ret= client1.publish("commands","on")                   #publish
+
+while True:
+    ret = client1.publish("command_1","forward")                   #publish
+    time.sleep(1)
