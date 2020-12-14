bc5debac29a9d112aa3e8a2e7004ec89a1cf38d9
ericwu13 <b04901074@ntu.edu.tw>
2020-12-14 02:19:55 -0800

    [Eric] refresh library

--- nrf/software/apps/recognition_v1/lib_gesture_flash.c
+++ nrf/software/apps/recognition_v1/lib_gesture_flash.c
@@ -5,8 +5,8 @@ const float thresholds[LIBRARY_SIZE] = {25,25,25, //F
 										25,25,25, //H
                                         25,25,30, //R
 										25,25,25, //L
-                                        25,25,25, //G
-										25,25,25, //U
+                                        20,20,20, //G
+										15,15,15, //U
                                         25,25,25};
 
 const char  gestures  [LIBRARY_SIZE] = "FFFBBBHHHRRRLLLGGGUUUSSS";
