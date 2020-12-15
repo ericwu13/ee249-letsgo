3babd6d6533843d263770346b61652cd3f76dc17
ericwu13 <b04901074@ntu.edu.tw>
2020-12-15 00:41:45 -0800

    collect len = 10

--- nrf/software/apps/data_collection/main.c
+++ nrf/software/apps/data_collection/main.c
@@ -41,7 +41,7 @@
 #include "app_util_platform.h"
 
 #define NUM_IMU_DATA 13
-#define MAX_SIGNAL_LENGTH 5
+#define MAX_SIGNAL_LENGTH 10
 static float IMU_data[NUM_IMU_DATA];
 static bool volatile moved = false;
 static int counter = 0;
